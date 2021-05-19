b452d1cbd906378833880e9c6e38d8881a0d4c7d
Odarlon Cardoso <62728490+odarlon@users.noreply.github.com>
2021-05-18 21:20:36 -0300

    Completando a tarefa
    Atualizando o main

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,26 @@
 const meuCabecalho = document.querySelector('h1');
 meuCabecalho.textContent = 'Hello world!';
+let meuBotao = document.querySelector('button');
+let meuCabecalho = document.querySelector('h1');
+function defineNomeUsuario() {
+    let meuNome = prompt('Please, enter your name.');
+    localStorage.setItem('nome', meuNome);
+    meuCabecalho.textContent = 'Mozilla is cool, ' + meuNome;
+  }
+  if(!localStorage.getItem('nome')) {
+    defineNomeUsuario();
+  } else {
+    let nomeGuardado = localStorage.getItem('nome');
+    meuCabecalho.textContent = 'Mozilla is cool, ' + nomeGuardado;
+  }
+  meuBotao.onclick = function() { defineNomeUsuario();
+  }
+  function defineNomeUsuario() {
+    let meuNome = prompt('Please, enter your name.');
+    if(!meuNome || meuNome === null) {
+      defineNomeUsuario();
+    } else {
+      localStorage.setItem('nome', meuNome);
+      meuCabecalho.innerHTML = 'Mozozilla is cool, ' + meuNome;
+    }
+  }
